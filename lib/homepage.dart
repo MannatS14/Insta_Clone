8f1ba4365d6d5a224a260399420833202f308701
Mannat Singh <mannatsingh@Mannats-MacBook-Air.local>
2024-03-27 23:55:44 +0530

    Added Screens

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,11 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:insta_clone/pages/home.dart';
+import 'package:insta_clone/pages/profile.dart';
+import 'package:insta_clone/pages/reels.dart';
+import 'package:insta_clone/pages/search.dart';
+import 'package:insta_clone/pages/shop.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,11 +15,29 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0; //navigation
+
+  void _navigateBottomNavBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<Widget> _pages = [
+    UserHome(),
+    UserSearch(),
+    UserReels(),
+    UserShop(),
+    UserProfile(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(child: Text('body here')),
+      body: _pages[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _navigateBottomNavBar,
         type: BottomNavigationBarType.fixed,
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
